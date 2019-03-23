dba7d54ab4dd6975fffc91bf5a0c9f7568d20bf6
moriyuu <moriyu54321@gmail.com>
2019-03-23 17:45:26 +0900

    on boarding 閉じれるように

--- components/OnBoarding.js
+++ components/OnBoarding.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ScrollView, Text, Button, Dimensions } from "react-native";
 import styled from "styled-components/native";
+import Modal from "react-native-modalbox";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
@@ -11,32 +12,41 @@ const Pane = styled.View`
   justify-content: center;
 `;
 
-const OnBoarding = () => {
-  return (
-    <ScrollView
-      horizontal
-      pagingEnabled
-      style={{ flex: 1, backgroundColor: "#eee" }}
-    >
-      <Pane>
-        <Text>flor</Text>
-        <Text>１輪の花を届けよう</Text>
-      </Pane>
-      <Pane>
-        <Text>田舎のおばあちゃんに。</Text>
-        <Text>遠距離の恋人に。</Text>
-        <Text>卒業する先輩に。</Text>
-        <Text>たまにしか会えない実家の両親に。</Text>
-      </Pane>
-      <Pane>
-        <Text>お花を選んで気軽に贈ろう</Text>
-      </Pane>
-      <Pane>
-        <Text>あなたの想いを届けよう</Text>
-        <Button title="はじめる" onPress={() => console.log("start")} />
-      </Pane>
-    </ScrollView>
-  );
-};
+class OnBoarding extends React.Component {
+  state = { isOpen: true };
+
+  render() {
+    return (
+      <Modal isOpen={this.state.isOpen} swipeToClose={false}>
+        <ScrollView
+          horizontal
+          pagingEnabled
+          style={{ flex: 1, backgroundColor: "#eee" }}
+        >
+          <Pane>
+            <Text>flor</Text>
+            <Text>１輪の花を届けよう</Text>
+          </Pane>
+          <Pane>
+            <Text>田舎のおばあちゃんに。</Text>
+            <Text>遠距離の恋人に。</Text>
+            <Text>卒業する先輩に。</Text>
+            <Text>たまにしか会えない実家の両親に。</Text>
+          </Pane>
+          <Pane>
+            <Text>お花を選んで気軽に贈ろう</Text>
+          </Pane>
+          <Pane>
+            <Text>あなたの想いを届けよう</Text>
+            <Button
+              title="はじめる"
+              onPress={() => this.setState({ isOpen: false })}
+            />
+          </Pane>
+        </ScrollView>
+      </Modal>
+    );
+  }
+}
 
 export default OnBoarding;
